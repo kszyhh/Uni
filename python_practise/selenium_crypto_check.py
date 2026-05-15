60ac17fb25699703ec5bffaf42aadaa554d3945f
kszyhh <kszyh@student.agh.edu.pl>
2026-05-15 17:16:41 +0200

    added driver.quit()

--- python_practise/selenium_crypto_check.py
+++ python_practise/selenium_crypto_check.py
@@ -14,24 +14,28 @@ driver = webdriver.Firefox(options=options)
 
 cryptos = ['BTC','SOL','XMR']
 
-while (True):
-    
-    try:
-        for i in cryptos:
-            now = datetime.now()
-            driver.get(f"\nhttps://www.google.com/finance/beta/quote/{i}-USD")    
-            name = driver.find_element(By.CLASS_NAME,'gO24Ff')
-            price = driver.find_element(By.CLASS_NAME,'N6SYTe')
-            print(now.strftime("%Y-%m-%d %H:%M"), f"{name.text} {price.text}\n")
-        print("=" * 50)
-        time.sleep(30)
-        driver.refresh()
-            
+try:
+    while (True):
+        
+        try:
+            for i in cryptos:
+                now = datetime.now()
+                driver.get(f"\nhttps://www.google.com/finance/beta/quote/{i}-USD")    
+                name = driver.find_element(By.CLASS_NAME,'gO24Ff')
+                price = driver.find_element(By.CLASS_NAME,'N6SYTe')
+                print(now.strftime("%Y-%m-%d %H:%M"), f"{name.text} {price.text}\n")
+            print("=" * 50)
+            time.sleep(30)
+            driver.refresh()
+                
 
-    except Exception as e:
-        print("An error has occured while downloading the data at ", datetime.now() , e)
-        time.sleep(30)
-        driver.refresh()
-    
+        except Exception as e:
+            print("An error has occured while downloading the data at ", datetime.now() , e)
+            time.sleep(30)
+            driver.refresh()
 
-#driver.quit()
+except KeyboardInterrupt:
+    pass
+
+finally:
+    driver.quit()
